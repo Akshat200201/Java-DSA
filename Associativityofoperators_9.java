b214b24c25d791b2c0a008109bd3c10d473ef41a
Akshat01 <94279138+Akshat200201@users.noreply.github.com>
2023-05-21 14:25:21 +0530

    Add files via upload

---
+++ Associativityofoperators_9.java
@@ -0,0 +1,21 @@
+public class Associativityofoperators_9 {
+    public static void main(String[] args) {
+//        int a= 6*5-34/2;
+//        int b= 34/2-6*5;
+//        System.out.println(a);
+//        System.out.println(b);
+        //presisdence and assiciativity
+//         quick quiz
+
+        int x=6;
+        int y = 1;
+        int z=2;
+
+//        int k= x*y/2;
+        int k = y*y - (4*x*z)/(2*x);
+        System.out.println(k);
+
+
+    }
+
+}
