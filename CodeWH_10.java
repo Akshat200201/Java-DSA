cdd3d91e83e8cb3f3463dbde40bd06ff4beea436
Akshat01 <94279138+Akshat200201@users.noreply.github.com>
2023-05-22 10:49:03 +0530

    Add files via upload

---
+++ CodeWH_10.java
@@ -0,0 +1,22 @@
+public class CodeWH_10 {
+    public static void main(String[] args) {
+//        int a= 4+5;
+//        float b= 3.556f +6;
+
+
+//        increment and decrement operator
+//        int i=56;
+//        System.out.println(i++);
+//        System.out.println(i);
+//        System.out.println(++i);
+//        System.out.println(i);
+
+        int y=7;
+        int x= ++y*8;
+        System.out.println(x);
+        char ch ='v';
+        System.out.println(++ch);
+
+
+    }
+}
---
+++ codeWH_7.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class codeWH_7 {
+    public static void main(String[] args) {
+//        Question 1
+//        int a=4;
+//        int b=5;
+//        int c= 8;
+//        int sum= a+b+c;
+//        System.out.println(sum);
+
+//        Question 2
+//        float subject1=45;
+//        float subject2=95;
+//        float subject3=67;
+//        float cgpa=(subject1+subject2+subject3)/30;
+//        System.out.println(cgpa);
+
+//        Question3
+//        System.out.println("what is your name");
+//        Scanner sc=new Scanner(System.in);
+//        String name= sc.next();
+//        System.out.println("hello " + name + " have a good day");
+
+//        question3
+//
+//        System.out.println("convert km to miles ::");
+//        Scanner sc= new Scanner(System.in);
+//        Float km = sc.nextFloat();
+//        System.out.println("convert "+ km+ " km to  miles");
+
+//        qustion 5
+        System.out.println("enter your number");
+        Scanner sc= new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+}
