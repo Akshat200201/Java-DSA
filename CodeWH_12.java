a80d7f8deefac49b2c7992b7169153200f30d1d6
Akshat01 <94279138+Akshat200201@users.noreply.github.com>
2023-05-23 10:45:08 +0530

    Add files via upload

---
+++ CodeWH_12.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CodeWH_12 {
+    public static void main(String[] args) {
+//        question1
+//         float  a= 7/4.0f *9/2.0f ;
+//        System.out.println(a);
+
+//        question2
+//         char grade='B';
+//         grade = (char)(grade+8);
+//        System.out.println(grade);
+//        //decripting the grade
+//        grade=(char) (grade-8);
+//        System.out.println(grade);
+
+//            question3
+//        Scanner sc= new Scanner(System.in);
+//            int a= sc.nextInt();
+//        System.out.println(a>8);
+
+
+
+
+
+
+    }
+
+}
