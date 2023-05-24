3c188f0e7b0a9c9a329d85e16040ad15d9b45356
Akshat01 <94279138+Akshat200201@users.noreply.github.com>
2023-05-24 22:51:57 +0530

    Add files via upload

---
+++ do_while_loop_22.java
@@ -0,0 +1,18 @@
+public class do_while_loop_22 {
+    public static void main(String[] args) {
+//      int a=10;
+//      while (a<5){
+//          System.out.println(a);
+//          a++;
+//
+//      }
+        int b=10;
+      do{
+          System.out.println(b);
+          b++;
+      }
+      while(b<5);
+
+
+    }
+}
